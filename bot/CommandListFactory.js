05fb3a736aa99ef435f8801315a7d1daefd27b72
itai <chessandpotatoes@gmail.com>
2018-12-05 22:44:47 -0500

    Added tons of commands to fisherbot

--- bot/CommandListFactory.js
+++ bot/CommandListFactory.js
@@ -82,6 +82,201 @@ var commands = [
 					+ ". Could you mean '" + prefix + "clip" + delimiter + " play" + delimiter + " " + action + "' ?");
 			}
 		}
+	},
+	{
+		name: "commandList",
+		arguments: 0,
+		description: "Lists all accessable commands.",
+		isAdminCommand: false,
+		method: function(message,args){
+			let commandList = [];
+			for(var i = 0; i < commands.length; i++){
+				if(!commands[i].isAdminCommand){
+					commandList.push(commands[i].name);
+				}
+			}
+			commandList.sort();//sort alphabetically
+			
+			let commandsString = "Command List: \n";
+			for(var i = 0; i < commandList.length; i++){
+				commandsString += (i+1) + ")\t" + commandList[i] + "\n";
+			}
+			commandsString += "* Use " + prefix + "help with a command to see its description, and " + prefix + "syntax to view the current syntax for commands. *";
+			message.channel.send(commandsString);
+		}
+	},
+	{
+		name: "help",
+		arguments: 1,
+		description: "Explains what a certain command does. Example: " + prefix + "help" + delimiter + "ping.",
+		isAdminCommand: false,
+		method: function(message,args){
+			var commandName = args[0].trim().toLowerCase();
+			let failFlag = true;
+			
+			
+			for(var i = 0; i < commands.length; i++){
+				if(commands[i].name.toLowerCase() == commandName){
+					message.channel.send(commands[i].description);
+					failFlag = false;
+					break;
+				}
+			}
+
+			if(failFlag){
+				message.channel.send("No such command exists.");
+			}
+		}
+	},
+	{ 
+		name: "roll",
+		arguments: 1,
+		description: "Rolls a die with x sides. Example: " + prefix + "roll" + delimiter + "6",
+		isAdminCommand: false,
+		method: function(message,args){
+			var numberOfSides = args[0];
+			message.channel.sendMessage("I rolled a " + (Math.floor(Math.random() * numberOfSides) + 1) + ".");
+		}
+	},
+	{
+		name: "avatar",
+		arguments: 1,
+		description: "Gets the avatar of the listed user. Example: " + prefix + "avatar" + delimiter + "@FisherBot#0187",
+		isAdminCommand: false,
+		method: function(message,args){
+			if(message.mentions.users.first() != null){
+				message.channel.send(message.mentions.users.first().avatarURL);
+			}else{
+				message.channel.send("No user was mentioned. You can mention a user with @ + their username.");
+			}
+		}
+	},
+	{
+		name: "timestamp",
+		arguments: 0,
+		description: "Gets the current timestamp.",
+		isAdminCommand: false,
+		method: function(message,args){
+			message.channel.send(timeStamp());
+		}
+	},
+	{		
+		name: "purge",
+		arguments: 1,
+		description: "Deletes a certain number of recent messages in the chat. Example: " + prefix + "purge" + delimiter +  "2",
+		isAdminCommand: true,
+		method: function(message,args){
+			let numMessages = Math.abs(parseInt(args[0]))+1;
+			if(numMessages > 20){
+				message.channel.send("You may not purge more than 20 messages at once.");
+			}else{
+				message.channel.fetchMessages({limit: numMessages}).then(messages => message.channel.bulkDelete(messages));
+			}
+		}
+	},
+	{
+		name: "remindme",
+		arguments: 2,
+		description: "Sends a user a reminder after x minutes. WARNING: WILL NOT REMIND YOU IF BOT IS UNDERGOING MAINTENANCE! Example: " + prefix + "remindme" + delimiter+ "10" + delimiter + "Wash the Dishes!",
+		isAdminCommand: false,
+		method: function(message,args){
+			
+			let numMins = parseFloat(args[0]);
+			let remindMessage = args[1];
+			if(numMins <= 35791){
+				message.channel.send("You will be reminded in " + numMins + " minutes.");
+				setTimeout(
+					function(){
+						message.reply(remindMessage);
+					},
+					numMins*60*1000//Mins->Seconds->Miliseconds
+				);
+			}else{
+				message.channel.send("That is too many minutes!");
+			}
+		}
+	},
+	{
+		name: "syntax",
+		arguments: 0,
+		description: "Displays the current syntax that Fisherbot uses.",
+		isAdminCommand: false,
+		method: function(message,args){
+			message.channel.send("The current Delimiter is: \'" + delimiter + "\'\nThe current command prefix is: \'" + prefix + "\'\nExample: " + prefix + "roll" + delimiter + "6");
+		}
+	},
+	{
+		name: "flip",
+		arguments: 0,
+		description: "Flips a coin.",
+		isAdminCommand: false,
+		method: function(message,args){
+			var rand = Math.random();
+			if(rand > 0.5){
+				message.channel.sendMessage("Heads");
+			}else{
+				message.channel.sendMessage("Tails");
+			}
+		}
+	},
+	{
+		name: "getid",
+		arguments: 0,
+		description: "Gets the user's ID (mostly for debugging purposes)",
+		isAdminCommand: false,
+		method: function(message,args){
+			message.channel.sendMessage("This ID for " + message.author.username + " is " + message.author.id);
+		}
+	},
+	{
+		name: "intown",
+		arguments: 0,
+		description: "Sets the user to be in town",
+		isAdminCommand: false,
+		method: function(message,args){
+			if(!message.member.roles.has(message.guild.roles.find("name", "In Town").id)) {
+				let roles = message.guild.roles;//message.author.addRole(role).catch(console.error);
+				let roleIDs = Array.from(roles.keys());
+				let newRoleArray = [];
+				for(var i = 0; i < roleIDs.length; i++){
+					let currentRole = roleIDs[i];
+					if(message.member.roles.has(currentRole) && currentRole != message.guild.roles.find("name", "Out of Town").id){
+						newRoleArray.push(currentRole + "");
+					}
+				}
+				newRoleArray.push(message.guild.roles.find("name", "In Town").id);
+
+				message.member.setRoles(newRoleArray);
+				message.channel.sendMessage("You are now set to be 'In Town'");
+			}else{
+				message.channel.sendMessage("You are already set as 'In Town'");
+			}
+		}
+	},
+	{
+		name: "outtown",
+		arguments: 0,
+		description: "Sets the user to be out of town",
+		isAdminCommand: false,
+		method: function(message,args){
+			if(!message.member.roles.has(message.guild.roles.find("name", "Out of Town").id)) {
+				let roles = message.guild.roles;//message.author.addRole(role).catch(console.error);
+				let roleIDs = Array.from(roles.keys());
+				let newRoleArray = [];
+				for(var i = 0; i < roleIDs.length; i++){
+					let currentRole = roleIDs[i];
+					if(message.member.roles.has(currentRole) && currentRole != message.guild.roles.find("name", "In Town").id){
+						newRoleArray.push(currentRole + "");
+					}
+				}
+				newRoleArray.push(message.guild.roles.find("name", "Out of Town").id);
+
+				message.member.setRoles(newRoleArray);
+				message.channel.sendMessage("You are now set to be 'Out of Town'");
+			}else{
+				message.channel.sendMessage("You are already set as 'Out of Town'");
+			}
+		}
 	}
 ];
 
