3586d6ee5d4574f0f78278c6018f8b04e5091cae
itai <chessandpotatoes@gmail.com>
2018-12-05 23:21:41 -0500

    Changed run to command to close previous background processes

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+a=$(ps aux | grep 'node ./bot/fisherbot.js' | grep -v grep | awk '{print $2}')
+if [ -z "$a" ]
+then
+	echo "No process to stop"
+else
+	echo "Stopping current process $a"
+	kill $a 
+fi
 node ./bot/fisherbot.js > ./log/stdout.txt 2> ./log/stderr.txt &
+exit 0
