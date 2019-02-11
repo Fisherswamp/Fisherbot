076b1d22789566916edaa9b416b5ada6e500f599
itai <chessandpotatoes@gmail.com>
2019-02-11 16:18:54 +0000

    Made fisherbot output look more professional

--- bot/CommandListFactory.js
+++ bot/CommandListFactory.js
@@ -14,7 +14,7 @@ const commands = [
 		description: "Responds with pong, whenever the user enters \'ping\'.",
 		isAdminCommand: false,
 		method: function(message,args){
-			message.channel.send("pong");
+			sendMessage(message,"pong");
 		}
 	},
 	{
@@ -29,7 +29,7 @@ const commands = [
   			// we ignore it
 			if (!message.guild) return;
 			if(args.length == 0){
-				message.channel.send("Unable to process the 'clip' command with 0 arguments."		
+				sendMessage(message,"Unable to process the 'clip' command with 0 arguments."		
 							+ " Use '" + prefix + "help" + delimiter + "clip' to see correct syntax."
 					);
 				return;
@@ -42,7 +42,7 @@ const commands = [
 
 			if(action == 'play'){
 				if(args.length != 2){
-					message.channel.send("I am not sure what clip you would like me to play." 
+					sendMessage(message,"I am not sure what clip you would like me to play." 
 								+ " Use '" + prefix + "help" + delimiter + "clip' to see correct syntax."
 						);
 				}else{
@@ -50,37 +50,37 @@ const commands = [
 					if(audioDataDictionary[song]){	
 						playClip(audioDataDictionary[song], message);
 					}else{
-						message.channel.send("Unable to find clip " + song);
+						sendMessage(message,"Unable to find clip " + song);
 					}
 				}
 			}else if(action == 'stop'){
 				if(args.length != 1){
-					message.channel.send("Incorrect command usage. Try: " + prefix + "clip" + delimiter + " " + action);
+					sendMessage(message,"Incorrect command usage. Try: " + prefix + "clip" + delimiter + " " + action);
 				}else{
 					playClip(null, message);
 				}
 			}else if(action == 'list'){	
 				if(args.length != 1){
-					message.channel.send("Incorrect command usage. Try: " + prefix + "clip" + delimiter + " " + action);
+					sendMessage(message,"Incorrect command usage. Try: " + prefix + "clip" + delimiter + " " + action);
 				}else{
-					message.channel.send(Object.keys(audioDataDictionary));
+					sendMessage(message,Object.keys(audioDataDictionary));
 				}
 			}else if(action == 'describe'){
 				if(args.length != 2){
-					message.channel.send("I am not sure what clip you would like me to describe." 
+					sendMessage(message,"I am not sure what clip you would like me to describe." 
 								+ " Use '" + prefix + "help" + delimiter + "clip' to see correct syntax."
 						);
 				}else{
 					let song = args[1].trim();
 					if(audioDataDictionary[song]){
-						message.channel.send(song + ": " + audioDataDictionary[song].description);
+						sendMessage(message,song + ": " + audioDataDictionary[song].description);
 					}else{
-						message.channel.send("Unable to find clip " + song);
+						sendMessage(message,"Unable to find clip " + song);
 					}
 
 				}
 			}else{
-				message.channel.send("Unknown argument " + action
+				sendMessage(message,"Unknown argument " + action
 					+ ". Could you mean '" + prefix + "clip" + delimiter + " play" + delimiter + " " + action + "' ?");
 			}
 		}
@@ -104,7 +104,7 @@ const commands = [
 				commandsString += (i+1) + ")\t" + commandList[i] + "\n";
 			}
 			commandsString += "* Use " + prefix + "help with a command to see its description, and " + prefix + "syntax to view the current syntax for commands. *";
-			message.channel.send(commandsString);
+			sendMessage(message,commandsString);
 		}
 	},
 	{
@@ -119,14 +119,14 @@ const commands = [
 			
 			for(var i = 0; i < commands.length; i++){
 				if(commands[i].name.toLowerCase() == commandName){
-					message.channel.send(commands[i].description);
+					sendMessage(message,commands[i].description);
 					failFlag = false;
 					break;
 				}
 			}
 
 			if(failFlag){
-				message.channel.send("No such command exists.");
+				sendMessage(message,"No such command exists.");
 			}
 		}
 	},
@@ -137,7 +137,7 @@ const commands = [
 		isAdminCommand: false,
 		method: function(message,args){
 			var numberOfSides = args[0];
-			message.channel.send("I rolled a " + (Math.floor(Math.random() * numberOfSides) + 1) + ".");
+			sendMessage(message,"I rolled a " + (Math.floor(Math.random() * numberOfSides) + 1) + ".");
 		}
 	},
 	{
@@ -147,9 +147,9 @@ const commands = [
 		isAdminCommand: false,
 		method: function(message,args){
 			if(message.mentions.users.first() != null){
-				message.channel.send(message.mentions.users.first().avatarURL);
+				sendMessage(message,message.mentions.users.first().avatarURL);
 			}else{
-				message.channel.send("No user was mentioned. You can mention a user with @ + their username.");
+				sendMessage(message,"No user was mentioned. You can mention a user with @ + their username.");
 			}
 		}
 	},
@@ -159,7 +159,7 @@ const commands = [
 		description: "Gets the current timestamp.",
 		isAdminCommand: false,
 		method: function(message,args){
-			message.channel.send(timeStamp());
+			sendMessage(message,timeStamp());
 		}
 	},
 	{		
@@ -170,7 +170,7 @@ const commands = [
 		method: function(message,args){
 			let numMessages = Math.abs(parseInt(args[0]))+1;
 			if(numMessages > 20){
-				message.channel.send("You may not purge more than 20 messages at once.");
+				sendMessage(message,"You may not purge more than 20 messages at once.");
 			}else{
 				message.channel.fetchMessages({limit: numMessages}).then(messages => message.channel.bulkDelete(messages));
 			}
@@ -186,7 +186,7 @@ const commands = [
 			let numMins = parseFloat(args[0]);
 			let remindMessage = args[1];
 			if(numMins <= 35791){
-				message.channel.send("You will be reminded in " + numMins + " minutes.");
+				sendMessage(message,"You will be reminded in " + numMins + " minutes.");
 				setTimeout(
 					function(){
 						message.reply(remindMessage);
@@ -194,7 +194,7 @@ const commands = [
 					numMins*60*1000//Mins->Seconds->Miliseconds
 				);
 			}else{
-				message.channel.send("That is too many minutes!");
+				sendMessage(message,"That is too many minutes!");
 			}
 		}
 	},
@@ -204,7 +204,7 @@ const commands = [
 		description: "Displays the current syntax that Fisherbot uses.",
 		isAdminCommand: false,
 		method: function(message,args){
-			message.channel.send("The current Delimiter is: \'" + delimiter + "\'\nThe current command prefix is: \'" + prefix + "\'\nExample: " + prefix + "roll" + delimiter + "6");
+			sendMessage(message,"The current Delimiter is: \'" + delimiter + "\'\nThe current command prefix is: \'" + prefix + "\'\nExample: " + prefix + "roll" + delimiter + "6");
 		}
 	},
 	{
@@ -215,9 +215,9 @@ const commands = [
 		method: function(message,args){
 			var rand = Math.random();
 			if(rand > 0.5){
-				message.channel.send("Heads");
+				sendMessage(message,"Heads");
 			}else{
-				message.channel.send("Tails");
+				sendMessage(message,"Tails");
 			}
 		}
 	},
@@ -227,7 +227,7 @@ const commands = [
 		description: "Gets the user's ID (mostly for debugging purposes)",
 		isAdminCommand: false,
 		method: function(message,args){
-			message.channel.send("This ID for " + message.author.username + " is " + message.author.id);
+			sendMessage(message,"This ID for " + message.author.username + " is " + message.author.id);
 		}
 	},
 	{
@@ -249,9 +249,9 @@ const commands = [
 				newRoleArray.push(message.guild.roles.find(x => x.name === "In Town").id);
 
 				message.member.setRoles(newRoleArray);
-				message.channel.send("You are now set to be 'In Town'");
+				sendMessage(message,"You are now set to be 'In Town'");
 			}else{
-				message.channel.send("You are already set as 'In Town'");
+				sendMessage(message,"You are already set as 'In Town'");
 			}
 		}
 	},
@@ -274,9 +274,9 @@ const commands = [
 				newRoleArray.push(message.guild.roles.find(x => x.name === "Out of Town").id);
 
 				message.member.setRoles(newRoleArray);
-				message.channel.send("You are now set to be 'Out of Town'");
+				sendMessage(message,"You are now set to be 'Out of Town'");
 			}else{
-				message.channel.send("You are already set as 'Out of Town'");
+				sendMessage(message,"You are already set as 'Out of Town'");
 			}
 		}
 	},
@@ -295,9 +295,9 @@ const commands = [
 						command += delimiter;
 					}
 				}
-				message.channel.send(command + " = " + Math.eval(command));
+				sendMessage(message,command + " = " + Math.eval(command));
 			} catch(err){
-				message.channel.send("Unable to parse '" + command + "'");
+				sendMessage(message,"Unable to parse '" + command + "'");
 			}
 		}
 
@@ -310,7 +310,7 @@ const commands = [
 		method: function(message,args){
 			let numMessages = Math.abs(parseInt(args[0]))+1;
 			if(numMessages > 20){
-				message.channel.send("You may not purge more than 20 messages at once.");
+				sendMessage(message,"You may not purge more than 20 messages at once.");
 			}else{
 				message.channel.fetchMessages({limit: numMessages}).then(messages => message.channel.bulkDelete(messages));
 			}
@@ -341,24 +341,24 @@ const commands = [
 //		description: "N/A",
 //		isAdminCommand: false,
 //		method: function(message,args){
-//			message.channel.send("\u200F hey ");
+//			sendMessage(message,"\u200F hey ");
 //		}
 //	}
 ];
 
 function playClip(songData, message){
 	if(message.member == null){
-		message.channel.send("This command does not work unless it is in a server.");
+		sendMessage(message,"This command does not work unless it is in a server.");
 		return;
 	}
 	ch = message.member.voiceChannel;
 	
 	if(songData == null){
-		message.channel.send("This function is still in development.");
+		sendMessage(message,"This function is still in development.");
 		return;
 	}
 	if(ch == null){
-		message.channel.send("Please join a channel first.");
+		sendMessage(message,"Please join a channel first.");
 	}else{
 		message.member.voiceChannel.join().then(connection => {
 			let pathString = Path.resolve(__dirname, "../audio_clips/" + songData.file_name);
@@ -381,7 +381,7 @@ function playClip(songData, message){
 			dispatcher.setVolume(1);
 		}).catch(
 			function(error){
-				message.channel.send("I have encountered an error trying to play this clip. Please ask my developer to check the error logs.");
+				sendMessage(message,"I have encountered an error trying to play this clip. Please ask my developer to check the error logs.");
 				console.log(error);
 			}
 		);
@@ -390,19 +390,19 @@ function playClip(songData, message){
 
 function playChesterClip(message){
 	if(message.member == null){
-		message.channel.send("This command does not work unless it is in a server.");
+		sendMessage(message,"This command does not work unless it is in a server.");
 		return;
 	}
 	ch = message.member.voiceChannel;
 	if(ch == null){
-		message.channel.send("Please join a channel first");
+		sendMessage(message,"Please join a channel first");
 		return;
 	}
 	const chesterFolder = "../audio_clips/chester_sounds/";
 	FS.readdir(Path.resolve(__dirname, chesterFolder), (err, files) => {
 		if(err){
 			console.log(err);
-			message.channel.send("Unable to play chester clip, please ask the admin to check their logs.");
+			sendMessage(message,"Unable to play chester clip, please ask the admin to check their logs.");
 			return;
 		}
 		let chosenSong = files[Math.floor(Math.random()*files.length)];	
@@ -427,12 +427,16 @@ function playChesterClip(message){
 			dispatcher.setVolume(1);
 		}).catch(
 			function(error){
-				message.channel.send("I have encountered an error trying to play this clip. Please ask my developer to check the error logs.");
+				sendMessage(message,"I have encountered an error trying to play this clip. Please ask my developer to check the error logs.");
 				console.log(error);
 			}
 		);
 	});
 	
+}
+
+let sendMessage = (messageData, sendString) => {
+	messageData.channel.send("```" + sendString + "```");
 }
 
 (function(){
